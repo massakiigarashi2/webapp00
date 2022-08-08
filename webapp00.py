6f4a7b207ac204ae0f60891bdbf0e4d5a5d9af20
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2022-08-08 15:36:19 -0300

    Add files via upload

---
+++ webapp00.py
@@ -0,0 +1,16 @@
+# myFirstStreamlitApp.py
+  
+#import the library
+import streamlit as st
+  
+# Use st.title("") para adicionar um TÍTULO ao seu Web app
+st.title("MEU 1º WEB APP STREAMLIT")
+
+# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
+st.header("Cabeçalho")
+
+# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
+st.subheader("Sub Cabeçalho")
+
+# Use st.write("") para adicionar um texto ao seu Web app
+st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
