05c9eab42e032d9a7a9d9611ba512db669d23773
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2022-09-12 08:54:43 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
+st.title("Meu prof. é Incrível!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Cabeçalho")
