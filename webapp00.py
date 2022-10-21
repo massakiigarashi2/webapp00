49c477ba3865e4fe7586206b8a39b927a662a206
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2022-10-21 15:59:24 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("SENAI ZERBINI É NOTA 1000!")
+st.title("Bem vindo, EMEF/EJA Edson Luis Lima Souto! Vcs são NOTA 1000!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Abacaxi em tudo!")
