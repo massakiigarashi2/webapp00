20da312d5ea7ec86b824748effb5fd230ca448af
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2023-11-09 10:28:13 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -29,6 +29,46 @@ response = requests.get(complete_url)
 # python format data
 x = response.json()
 
+if x["cod"] != "404":
+ 
+    # store the value of "main"
+    # key in variable y
+    y = x["main"]
+ 
+    # store the value corresponding
+    # to the "temp" key of y
+    current_temperature = y["temp"]
+ 
+    # store the value corresponding
+    # to the "pressure" key of y
+    current_pressure = y["pressure"]
+ 
+    # store the value corresponding
+    # to the "humidity" key of y
+    current_humidity = y["humidity"]
+ 
+    # store the value of "weather"
+    # key in variable z
+    z = x["weather"]
+ 
+    # store the value corresponding 
+    # to the "description" key at 
+    # the 0th index of z
+    weather_description = z[0]["description"]
+ 
+    # print following values
+    st.write(" Temperature (in kelvin unit) = " +
+                    str(current_temperature) +
+          "\n atmospheric pressure (in hPa unit) = " +
+                    str(current_pressure) +
+          "\n humidity (in percentage) = " +
+                    str(current_humidity) +
+          "\n description = " +
+                    str(weather_description))
+ 
+else:
+    st.write(" City Not Found ")
+
+
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Previsão do tempo - 09/11/23 -")
-st.write(x)
