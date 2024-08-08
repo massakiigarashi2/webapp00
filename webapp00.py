ef01d7d7e6b70ee35a5db76a73183e3ad2105915
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-08-08 10:01:48 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,6 +1,9 @@
+import streamlit
 import streamlit as st
+from streamlit import title
+from streamlit import *
 
-st.title("Olá, Prof. Massaki!")
+title("Olá, Prof. Massaki!")
 st.header("Turma 2EI-S")
 st.subheader("Este pessoal é fera!")
 st.write("Vamos lá, pessoal!")
