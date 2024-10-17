bd13dbfb9672140efce0ec453134719f2f6583b4
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-17 12:47:38 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -13,3 +13,18 @@ st.subheader("Sub Cabeçalho")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+
+Divisor()
+    
+coluna1 = Colunas(3)
+with coluna1[0]:
+  Escrever("")
+  with coluna1[1]:
+    Escrever("ACTlib Versão 0.1", "titulo")
+    nome = Ler(rotulo = "Nome:", nmax=30, tipo="padrao", info="Inserção de Nome", autocompletar=None, na_mudanca=None, args=None, kwargs=None, placeholder="Não esqueça de preencher seu nome", desabilitada="falso", visibilidade="visivel")
+    if nome:     
+      Escrever("Seja Bem vinda(o), " + nome + "!")
+    with coluna1[2]:
+      Escrever("")
+
+MKD("A vida não é um conto de fadas, mas pode ser repleta de momentos mágicos!", alinhamento = "justificado", tamanho_fonte = 64, cor_fonte = cor)
