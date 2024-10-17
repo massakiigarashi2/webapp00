76f0d566d22f756c12b5b9381ae49b94fb7ac9d0
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-17 20:57:03 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,5 +1,10 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
+from ACTlib01 import *
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQtWlwpc4_SPunNRbY9MWEsLemJ4-kr7JK1OE8avUWMU7BULFMQNt6-bFkIsJ-_7nOvH3sFOSyOFkeb/pub?gid=1851101266&single=true&output=csv"
+db = Ler_GooglePlanilha(url, coluna_indice = None)
+db.columns = ['DataHora', 'Idade', 'Jundiaiense', 'Qualidade', 'Sonho']
+Escrever(db)
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("MEU 1º WEB APP STREAMLIT")
