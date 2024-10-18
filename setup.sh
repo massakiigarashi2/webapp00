37caee363b30082bfb151c85413800ce3548006d
riq0007 <henriquesaideoliveira2006@gmail.com>
2024-10-18 20:15:56 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"henriquesaideoliveira2006@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
