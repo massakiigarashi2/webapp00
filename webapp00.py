1cac0b108739158135dd404332c563b563c25c70
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-18 21:26:39 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -21,3 +21,5 @@ st.write("Como já deve ter percebido, o método st.write() é usado para escrit
 
 values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
 st.write("Values:", values)
+
+st.image("desenvolvimento.jpg", caption="TESTE_Inserir_IMAGEM")
