a3d17bf4e32cd7f8e567915d9945398b3b34bb6c
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-23 20:20:37 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,23 +1,5 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
-from ACTlib01 import *
-from pdfquery import PDFQuery
-
-pdf = PDFQuery('Colab12Passos.pdf')
-pdf.load()
-
-# Use CSS-like selectors to locate the elements
-text_elements = pdf.pq('LTTextLineHorizontal')
-
-# Extract the text from the elements
-text = [t.text for t in text_elements]
-
-st.write(text)
-
-#url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFwxxM13bxUC0dpyd0w0PxfZIrJ-hp4Px-R6rsTiG3c3n-89JApzA0jYJpU9vNfxeNCvtJ0Cg35KtO/pub?gid=556192647&single=true&output=csv"
-#db = Ler_GooglePlanilha(url)
-#db.fillna('', inplace=True)
-#Escrever(db)
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("MEU 1º WEB APP STREAMLIT")
@@ -30,24 +12,3 @@ st.subheader("Sub Cabeçalho")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-age = st.slider("Pergunta?", 0, 100, 15)
-st.write("I'm ", age, "years old")
-
-st.image("desenvolvimento.jpg", caption="TESTE_Inserir_IMAGEM")
-
-import streamlit as st
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
