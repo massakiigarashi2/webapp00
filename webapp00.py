f920a60c04b8e5f8d5dfb46de77ceb3ac2d3a557
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-22 22:09:01 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -23,3 +23,19 @@ age = st.slider("Pergunta?", 0, 100, 15)
 st.write("I'm ", age, "years old")
 
 st.image("desenvolvimento.jpg", caption="TESTE_Inserir_IMAGEM")
+
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
