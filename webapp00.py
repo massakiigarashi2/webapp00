8f6c27c5cf4c344be114aadd4311d23048c98603
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-23 22:01:22 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -12,3 +12,17 @@ st.subheader("Sub Cabeçalho")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
